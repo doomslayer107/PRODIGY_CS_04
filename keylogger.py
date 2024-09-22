17f339ddcc14c2d4ed8e2b3c1f812173617c4e2c
Dev <167743954+doomslayer107@users.noreply.github.com>
2024-09-23 03:02:51 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,11 +1,3 @@
-'''
-Task # 04
-Simple Keylogger
-Create a basic keylogger program that records and logs keystrokes. 
-Focus on logging the keys pressed and saving them to a file. 
-Note: Ethical considerations and permissions are crucial for 
-projects involving keyloggers.
-'''
 
 from pynput import keyboard
 import time
